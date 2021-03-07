8d3ce9b850af53d8ccb5315359ecec160948c516
tanquan <tanquan@microxiang.com>
2021-03-07 15:51:15 +0800

    SpringBoot应用搭建，配置服务器端口号和应用访问路径。

---
+++ src/main/java/com/ruoyi/MyRuoYiApplication.java
@@ -0,0 +1,11 @@
+package com.ruoyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRuoYiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyRuoYiApplication.class, args);
+    }
+}
